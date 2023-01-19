801292fb0f91e6aee423c09bb77d35ba59a6567a
Rakesh-sudo-stack <rakeshkumar1957830@gmail.com>
2023-01-19 21:56:18 +0530

    new commit

---
+++ assets/js/party.js
@@ -0,0 +1,30 @@
+let bgMusic = new Audio("../img/background-music.mp3");
+
+// Generates random five digit number
+let code;
+const numgenerator = () => {
+    let minm = 10000;
+    let maxm = 99999;
+    let code = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+    return code;
+}
+
+let partyPage = document.querySelector('.party-page');
+let partyCodes = document.querySelector('.party-codes');
+let joinPartyCode = document.querySelector('.join-party-code');
+let createPartyCode = document.querySelector('.create-party-code');
+
+const createPartyPage = () => {
+    partyPage.style.display = "none";
+    partyCodes.style.display = "flex";
+    joinPartyCode.style.display = "none";
+    let createPartyCode = numgenerator();
+    document.querySelector('#code').innerHTML = createPartyCode;
+    code = createPartyCode;
+}
+
+const joinPartyPage = () => {
+    partyCodes.style.display = "flex";
+    partyPage.style.display = "none";
+    createPartyCode.style.display = "none";
+}
