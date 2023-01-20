e1190f16de8475dc7cc7ad8a557a1fdb9fea9a6b
Rakesh-sudo-stack <rakeshkumar1957830@gmail.com>
2023-01-20 20:30:03 +0530

    socket.io work started

--- assets/js/party.js
+++ assets/js/party.js
@@ -1,10 +1,12 @@
+var socket = io();
+
 let bgMusic = new Audio("../img/background-music.mp3");
 
 // Generates random five digit number
 let code;
 const numgenerator = () => {
-    let minm = 10000;
-    let maxm = 99999;
+    let minm = 100;
+    let maxm = 999;
     let code = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
     return code;
 }
@@ -21,6 +23,7 @@ const createPartyPage = () => {
     let createPartyCode = numgenerator();
     document.querySelector('#code').innerHTML = createPartyCode;
     code = createPartyCode;
+    socket.emit('create-room',code);
 }
 
 const joinPartyPage = () => {
@@ -28,3 +31,18 @@ const joinPartyPage = () => {
     partyPage.style.display = "none";
     createPartyCode.style.display = "none";
 }
+
+const joinRoom = () => {
+    let code = document.querySelector('#roomcode').value;
+    console.log(code);
+    socket.emit('join-room',parseInt(code));
+}
+
+socket.on('room-not-found',()=>{
+    document.querySelector('#party-not-found-text').innerHTML = "Party does not exist.";
+    setTimeout(()=>{document.querySelector('#party-not-found-text').innerHTML = '';},2000)
+})
+
+socket.on('join-game',(code)=>{
+    console.log(`Joined game ${code}`);
+})
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const port = process.env.PORT || 3000;
 
@@ -19,6 +21,40 @@ app.get('/party', (req, res) => {
   res.sendFile(__dirname+'/views/party.html');
 });
 
+let rooms = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('create-room',(code)=>{
+    rooms[socket.id] = code;
+    socket.join(code);
+    console.log(rooms);
+  })
+  socket.on('join-room',(code)=>{
+    let roomFound = false;
+    for (const item of Object.entries(rooms)) {
+      if(item[1] == code){
+        roomFound = true;
+      }
+    };
+    
+    if(roomFound){
+      rooms[socket.id] = code;
+      socket.join(code);
+      console.log(rooms);
+      io.to(code).emit('join-game',code);
+    }else{
+      socket.emit('room-not-found')
+    }
+  })
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    console.log()
+    delete rooms[socket.id];
+    console.log(rooms);
+  });
+});
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
